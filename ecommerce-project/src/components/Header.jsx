4bfddc10566e118de486c42ccb12fb111c2eefaa
tejareddyqs <se22ucse189@mahindrauniversity.edu.in>
2025-07-14 22:14:45 +0530

    get cart from backend and display quantity

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 import './header.css'
-export function Header(){
-  return(
+export function Header({cart}){
+  let totalQuantity = 0;
+  cart.forEach((cartItem)=>{
+     totalQuantity += cartItem.quantity;
+  });
+return(
     <div className="header">
       <div className="left-section">
         <Link  to="/" className="header-link">
@@ -27,7 +31,7 @@ export function Header(){
 
         <Link className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
-          <div className="cart-quantity">3</div>
+          <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
         </Link>
       </div>
--- ecommerce-project/src/pages/HomePages.jsx
+++ ecommerce-project/src/pages/HomePages.jsx
@@ -4,23 +4,31 @@ import {Header} from '../components/Header'
 import './HomePage.css';
 export function HomePage(){
   const[products,setProducts]=useState([])
+  const[cart,setCart]=useState([])
   useEffect(()=>{
     axios.get('http://localhost:3000/api/products')
   .then((response)=>{
    setProducts(response.data);
    })
   },[]);
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/cart-Items')
+  .then((response)=>{
+   setCart(response.data);
+   })
+  },[])
  
   return(
     <>  
     <title>Homepage</title>
    
-   <Header />
+   <Header cart={cart}/>
     <div className="home-page">
       <div className="products-grid">
         {products.map((product)=>{
-           return ( <div className="product-container">
-          <div key={product.id}className="product-image-container">
+           return ( 
+           <div key={product.id} className="product-container">
+          <div className="product-image-container">
             <img className="product-image"
               src={product.image} />
           </div>

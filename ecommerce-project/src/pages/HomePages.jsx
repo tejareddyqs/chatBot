b4d3fca707d91790370cfb873c6fb7a33fb80355
tejareddyqs <se22ucse189@mahindrauniversity.edu.in>
2025-07-14 18:51:15 +0530

    fetch data from the backend

--- ecommerce-project/src/pages/HomePages.jsx
+++ ecommerce-project/src/pages/HomePages.jsx
@@ -2,6 +2,12 @@ import {products} from '../../starting-code/data/products'
 import {Header} from '../components/Header'
 import './HomePage.css';
 export function HomePage(){
+  fetch('http://localhost:3000/api/products')
+  .then((response)=>{
+   response.json().then((data)=>{
+     console.log(data);
+   })
+  })
   return(
     <>  
     <title>Homepage</title>

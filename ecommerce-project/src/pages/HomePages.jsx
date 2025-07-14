e3089434093384883870cbcd2e82170c2e6b326f
tejareddyqs <se22ucse189@mahindrauniversity.edu.in>
2025-07-14 19:12:26 +0530

    use data from the backend to generate htm

--- ecommerce-project/src/pages/HomePages.jsx
+++ ecommerce-project/src/pages/HomePages.jsx
@@ -1,12 +1,16 @@
 import axios from 'axios'
-import {products} from '../../starting-code/data/products'
+import { useEffect,useState } from 'react';
 import {Header} from '../components/Header'
 import './HomePage.css';
 export function HomePage(){
-  axios.get('http://localhost:3000/api/products')
+  const[products,setProducts]=useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/products')
   .then((response)=>{
-   console.log(response.data);
+   setProducts(response.data);
    })
+  },[]);
+ 
   return(
     <>  
     <title>Homepage</title>

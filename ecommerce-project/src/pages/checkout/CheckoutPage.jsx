16fa006eea6a939981e8198fcdb132c64af935e2
tejareddyqs <se22ucse189@mahindrauniversity.edu.in>
2025-07-16 22:29:36 +0530

    create update delivery option feature

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -4,7 +4,7 @@
  import './CheckoutPage.css'
  import { PaymentSummary } from './PaymentSummary';
  import { OrderSummary } from './OrderSummary';
- export function CheckoutPage({cart}){
+ export function CheckoutPage({cart,loadCart}){
 const[deliveryOptions,setDeliveryOptions]=useState([])
 const[paymentSummary,setPaymentSummary]=useState(null)
 useEffect(() => {
@@ -16,7 +16,7 @@ useEffect(() => {
     setPaymentSummary(response.data);
   };
   fetchCheckOutData();
-}, []);
+}, [cart]);
 
   return(
     <>
@@ -45,7 +45,7 @@ useEffect(() => {
       <div className="page-title">Review your order</div>
      
       <div className="checkout-grid">
-       <OrderSummary cart={cart} deliveryOptions={deliveryOptions}/>
+       <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
 
        <PaymentSummary paymentSummary={paymentSummary}/>
       </div>
--- ecommerce-project/src/pages/checkout/DeliveryOptions.jsx
+++ ecommerce-project/src/pages/checkout/DeliveryOptions.jsx
@@ -1,33 +1,51 @@
 import dayjs from "dayjs";
+import axios from "axios";
 import { formatMoney } from "../../utils/money";
-export function DeliveryOptions({deliveryOptions,cartItem}){
-  return(
-    
-                  <div className="delivery-options">
-                    <div className="delivery-options-title">
-                      Choose a delivery option:
-                    </div>
-                    {deliveryOptions.map((deliveryOption)=>{
-                      let priceString = 'FREE-Shipping';
-                      if(deliveryOption.priceCents>0){
-                        priceString = `${formatMoney(deliveryOption.priceCents)}-shipping`;
-                      }
-                      return(
-                         <div key={deliveryOption.id}className="delivery-option">
-                      <input type="radio" checked={deliveryOption.id ===cartItem.deliveryOptionId}
-                        className="delivery-option-input"
-                        name="`delivery-option-${cartItem.product.id}`" />
-                      <div>
-                        <div className="delivery-option-date">
-                          {dayjs(deliveryOptions.estimatedDeliveryOptionsMs).format('dddd, MMMM D')}
-                        </div>
-                        <div className="delivery-option-price">
-                          {priceString}
-                        </div>
-                      </div> 
-                    </div>
-                      )
-                    })}
-                  </div>
+
+export function DeliveryOptions({ deliveryOptions, cartItem, loadCart }) {
+  return (
+    <div className="delivery-options">
+      <div className="delivery-options-title">
+        Choose a delivery option:
+      </div>
+
+      {deliveryOptions.map((deliveryOption) => {
+        let priceString = "FREE-Shipping";
+        if (deliveryOption.priceCents > 0) {
+          priceString = `${formatMoney(deliveryOption.priceCents)} - Shipping`;
+        }
+
+        const updateDeliveryOption = async () => {
+          await axios.put(`/api/cart-items/${cartItem.productId}`, {
+            deliveryOptionId: deliveryOption.id
+          });
+          await loadCart();
+        };
+
+        return (
+          <div
+            key={deliveryOption.id}
+            className="delivery-option"
+            onClick={updateDeliveryOption}
+          >
+            <input
+              type="radio"
+              checked={deliveryOption.id === cartItem.deliveryOptionId}
+              className="delivery-option-input"
+              name={`delivery-option-${cartItem.product.id}`}
+              
+            />
+            <div>
+              <div className="delivery-option-date">
+                {dayjs(deliveryOption.estimatedDeliveryTimeMs).format(
+                  "dddd, MMMM D"
+                )}
+              </div>
+              <div className="delivery-option-price">{priceString}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }

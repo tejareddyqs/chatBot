eaa117a73aa808541951658d292ba61c21c1e313
tejareddyqs <se22ucse189@mahindrauniversity.edu.in>
2025-07-16 21:09:50 +0530

    create add to cart

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -8,16 +8,16 @@ import './App.css';
 
 function App() {
   const[cart,setCart]=useState([])
-   useEffect(()=>{
-    const getHomeData = async () => {
+   const loadCart = async () => {
   const response =await axios.get('/api/cart-Items?expand=product')
    setCart(response.data);
-    }
-   getHomeData();
+    } 
+  useEffect(()=>{
+   loadCart();
   },[]);
   return (
     <Routes>  
-      <Route index element={<HomePage cart={cart}/>} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart}/>} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path='Orders' element={<OrdersPage cart={cart}/>}/>
     </Routes>
